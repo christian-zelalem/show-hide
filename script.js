8a5d89821c69f30176b6f7aae4b5e7b980cf7499
christian-zelalem <christianzelalem24@gmail.com>
2023-04-21 00:52:15 -0800

    s
    show/hide

---
+++ script.js
@@ -0,0 +1,33 @@
+let hiden = document.querySelector('.hiden-content')
+let hidenBtn = document.querySelector('btn-1')
+
+function hidecontent(){
+ 
+    hiden.style.display = 'none'
+if(hiden.style.block='none'){
+  let showbtns = document.querySelector('.btn-2')
+      showbtns.style.display = 'block'
+      let hidbtn = document.querySelector('.btn-1')
+      hidbtn.style.display = 'none'
+}
+
+}
+
+function showcontent(){
+    hiden.style.display = 'block'
+    
+    
+    
+    if(hiden.style.display = 'block'){
+        let hide = document.querySelector('.btn-2')
+        hide.style.display = 'none'
+        let hiBtn = document.querySelector('.btn-1')
+        hiBtn.style.display = 'block'
+    }
+
+
+}
+
+
+
+
